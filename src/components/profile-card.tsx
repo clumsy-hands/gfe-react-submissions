c79eacf32e64db124b0136aacfe18caf59a4aed7
clumsy-hands <vipin.doon.2@gmail.com>
2025-04-03 20:49:44 +0200

    bug: fix color for button and icons

--- src/components/profile-card.tsx
+++ src/components/profile-card.tsx
@@ -1,3 +1,5 @@
+const LINK_CSS =
+  "text-indigo-700 w-[24px] h-[24px] flex items-center justify-center hover:text-indigo-900";
 export default function ProfileCard() {
   return (
     <div className="flex flex-row justify-center min-h-screen bg-gradient-to-br from-[#F9FAFB] to-[#D2D6DB] pt-[200px] w-full">
@@ -16,13 +18,13 @@ export default function ProfileCard() {
           Stack Overflow and ChatGPT developer.
         </p>
 
-        <button className="bg-violet-500 hover:bg-violet-50 hover:text-violet-500 text-white py-2 px-4 w-full my-5 cursor-pointer">
+        <button className="bg-indigo-700 hover:bg-indigo-50 hover:text-indigo-700 text-white py-2 px-4 w-full my-5 cursor-pointer">
           Contact me
         </button>
 
         <div className="flex flex-row gap-2">
           <a
-            className="text-violet-500 w-[24px] h-[24px] flex items-center justify-center hover:text-violet-900"
+            className={LINK_CSS}
             href="https://www.github.com/clumsy-hands"
             target="_blank"
           >
@@ -35,7 +37,7 @@ export default function ProfileCard() {
             </svg>
           </a>
           <a
-            className="text-violet-500 w-[24px] h-[24px] flex items-center justify-center hover:text-violet-900"
+            className={LINK_CSS}
             href="https://www.linkedin.com/in/vipin-bhandari-b021b9104/"
             target="_blank"
           >
@@ -47,11 +49,7 @@ export default function ProfileCard() {
               <path d="M18.3362 18.339H15.6707V14.1622C15.6707 13.1662 15.6505 11.8845 14.2817 11.8845C12.892 11.8845 12.6797 12.9683 12.6797 14.0887V18.339H10.0142V9.75H12.5747V10.9207H12.6092C12.967 10.2457 13.837 9.53325 15.1367 9.53325C17.8375 9.53325 18.337 11.3108 18.337 13.6245V18.339H18.3362ZM7.00373 8.57475C6.14573 8.57475 5.45648 7.88025 5.45648 7.026C5.45648 6.1725 6.14648 5.47875 7.00373 5.47875C7.85873 5.47875 8.55173 6.1725 8.55173 7.026C8.55173 7.88025 7.85798 8.57475 7.00373 8.57475ZM8.34023 18.339H5.66723V9.75H8.34023V18.339ZM19.6697 3H4.32923C3.59498 3 3.00098 3.5805 3.00098 4.29675V19.7033C3.00098 20.4202 3.59498 21 4.32923 21H19.6675C20.401 21 21.001 20.4202 21.001 19.7033V4.29675C21.001 3.5805 20.401 3 19.6675 3H19.6697Z"></path>
             </svg>
           </a>
-          <a
-            className="text-violet-500 w-[24px] h-[24px] flex items-center justify-center hover:text-violet-900"
-            href="https://instagram.com"
-            target="_blank"
-          >
+          <a className={LINK_CSS} href="https://instagram.com" target="_blank">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -61,7 +59,7 @@ export default function ProfileCard() {
             </svg>
           </a>
           <a
-            className="text-violet-500 w-[24px] h-[24px] flex items-center justify-center hover:text-violet-900"
+            className={LINK_CSS}
             href="https://www.twitter.com"
             target="_blank"
           >
